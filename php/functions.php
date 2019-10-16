17bad7ba02b671f710b27a48c74e32a220936a5c
Jaroslav Langer <langeja5@fit.cvut.cz>
2019-10-17 01:22:45 +0200

    More PHP, regex and more.

--- php/functions.php
+++ php/functions.php
@@ -23,3 +23,11 @@ print_r($matches); // [ "def" ]
 $string = "1+2-3";
 $array = preg_split('/[+-]/', $string);
 print_r($array);    // [1, 2, 3]
+
+# REGEX advanced
+// Matching numbers
+'/[[:digit:]]*/';
+// befor digits expects white space
+'1234'; // OK
+'1234abc'; // OK
+'abc1234'; // not found
