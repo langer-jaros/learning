047d712e8f0173fd0c758acbf23793121bd6a5cc
Jaroslav Langer <langeja5@fit.cvut.cz>
2020-10-14 00:15:27 +0200

    Combinatorial optimization homework turned in

--- informatics/combinatorial_optimization/homeworks/hw01/compute/compute.py
+++ informatics/combinatorial_optimization/homeworks/hw01/compute/compute.py
@@ -6,31 +6,23 @@ clib = ctypes.cdll.LoadLibrary("./build/knapsack.so")
 clib.solve.argtypes = [c_int, c_char_p, c_char_p]
 clib.solve.restype = c_double
 
-NUMBERS = [4, 10, 15, 20] # [4, 10, 15, 20, 22, 25, 27] # [4, 10, 15, 20, 22, 25, 27, 30, 32, 35, 37, 40]
-CONFIG = [
-    {
-        "inst": "../data/nr/NR{n}_inst.dat",
-        "resu": "../data/computed/nr_resu/bf_n_n{n}_resu_test.csv",
-        "method": 0
-    },
-    {
-        "inst": "../data/nr/NR{n}_inst.dat",
-        "resu": "../data/computed/nr_resu/bab_n_n{n}_resu_test.csv",
-        "method": 1,
-    },
-    {
-        "inst": "../data/zr/ZR{n}_inst.dat",
-        "resu": "../data/computed/zr_resu/bf_z_n{n}_resu_test.csv",
-        "method": 0,
-    },
-    {
-        "inst": "../data/zr/ZR{n}_inst.dat",
-        "resu": "../data/computed/zr_resu/bab_z_n{n}_resu_test.csv",
-        "method": 1,
-    }
+SETS = [
+    (
+        "../data/nr/NR{n}_inst.dat", 
+        "../data/computed/nr_resu/n_n{n}_{m}_resu_test.csv"
+    ),
+    (
+        "../data/zr/ZR{n}_inst.dat",
+        "../data/computed/zr_resu/z_n{n}_{m}_resu_test.csv"
+    )
 ]
 
-for conf in CONFIG:
-    for n in NUMBERS:
-        time = clib.solve(conf["method"], conf["inst"].format(n=n).encode(), conf["resu"].format(n=n).encode())
-        print("n:", n, "current time:", datetime.now().strftime("%H:%M:%S"))
+METHODS = {0: "bf", 1: "bab", 2: "sbab"}
+
+NUMBERS = [4, 10, 15, 20] # [4, 10, 15, 20, 22, 25, 27] # [4, 10, 15, 20, 22, 25, 27, 30, 32, 35, 37, 40]
+
+for inst, resu in SETS:
+    for m in METHODS:
+        for n in NUMBERS:
+            time = clib.solve(m, inst.format(n=n).encode(), resu.format(n=n, m=METHODS[m]).encode())
+            print("n:", n, "current time:", datetime.now().strftime("%H:%M:%S"))
