92be5d34016fd459fa253a5ee0ee95de287a853c
Jaroslav Langer <langeja5@fit.cvut.cz>
2020-11-07 21:08:52 +0100

    combinatorial_optimization/*/hw02: greedy_heuristic and redux implemented

--- informatics/combinatorial_optimization/homeworks/hw02/compute/scripts/test.bash
+++ informatics/combinatorial_optimization/homeworks/hw02/compute/scripts/test.bash
@@ -14,7 +14,7 @@ for ds in ${dataset[@]}; do
             tmp_file=${tmp_path}/${ds}/${ds^^}_${m#*:}_${n}.csv
             case $1 in
             samples)
-                shuf -n 5 ${inst_file} | sort -t ' ' -n > ${in_file}
+                shuf -n ${samples_num} ${inst_file} | sort -t ' ' -n > ${in_file}
                 id_patt=$(cut -d ' ' -f 1 ${in_file} | tr '\n' '|')
                 egrep "^(${id_patt:0:-1}) " ${sol_file} > ${out_file}
             ;;
@@ -26,6 +26,9 @@ for ds in ${dataset[@]}; do
             ;;
             dp)
                 $dp_cmd < $in_file > $tmp_file
+            ;;
+            gh)
+                $gh_cmd < $in_file > $tmp_file
             ;;
             show)
                 echo ${dir_data}/${ds}/${ds^^}_${n}
