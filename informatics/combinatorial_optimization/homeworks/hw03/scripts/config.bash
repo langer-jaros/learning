a53eb88a6c2cfd0d37363313236ccd1efd5e55ca
Jaroslav Langer <langeja5@fit.cvut.cz>
2020-11-26 20:14:15 +0100

    combinatorial_optimization/*/hw03: Homework done.

--- informatics/combinatorial_optimization/homeworks/hw03/scripts/config.bash
+++ informatics/combinatorial_optimization/homeworks/hw03/scripts/config.bash
@@ -11,7 +11,7 @@ knap=${project_path}/compute/build/knapsack
 
 # I     # Frist ID
 n=16    # number of items
-N=5     # number of instances
+N=100   # number of instances
 m=0.8   # ratio of capacity/total_weight
 W=5000  # max weight of an item 
 w=bal   # more light or heavy items, or balanced
@@ -20,7 +20,7 @@ c=uni   # weith,value correlation
 k=1     # granularity exponent
 d=1     # delta (number instances to be permutated)
 
-ns=(5) #(1 4 7 10 13 16 19 22 25 28)
+ns=(1 4 7 10 13 16 19 22 25 28)
 ms=(0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1)
 Ws=(1000 2000 3000 4000 5000 6000 7000 8000 9000 10000)
 ws=(bal light heavy)
@@ -30,4 +30,3 @@ ks=(0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1)
 ps=(0 1 2 3 4 5 6 7 7 8 9)
 
 methods=("1:bf" "2:bab" "3:dp" "4:gh" "5:redux")
-
